9232d0d13f5e40d930199336d2a7a0ee8ea33f4a
debarghasarkar175 <debarghasarkar175@gmail.com>
2025-11-14 20:09:35 +0000

    feat: create project pages and update projects list

---
+++ js/theme.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.querySelector('.theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+        let theme = 'light-theme';
+        if (document.body.classList.contains('dark-theme')) {
+            theme = 'dark-theme';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
